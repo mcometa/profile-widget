85089be647d15cbe49429c31147b3d0396867ec9
Marc Cometa <marc@mcometa.com>
2014-03-29 01:22:47 +0800

    Added JS start

--- js/app.js
+++ js/app.js
@@ -1,9 +1,43 @@
-var App = {
-  
-  init: function() {
-    console.log("App:init()");
-  }
+'use strict';
+var messages,
+    App = {
 
-};
+      messages: [
+        'How was the weather today? Was it sunny and perfect?',
+        'Did you get stuck at traffic? Tell us more about it.',
+        'What is on your mind?'
+      ],
+      
+      init: function() {
+        this.attachEvents();
+        console.log("App:init()");
+      },
+
+      attachEvents: function() {
+        var newPost   = document.querySelector('.js-new-post'),
+            likePost  = document.querySelector('.js-like-post'),
+            replyPost = document.querySelector('.js-reply-post');
+
+
+        newPost.addEventListener('click', this.newPost, false);
+        likePost.addEventListener('click', this.likePost, false);
+        replyPost.addEventListener('click', this.replyPost, false);
+      },
+
+      newPost: function() {
+        // Append a new post form in the posts container, replace the 'New'
+      },
+
+      showPrev: function() {
+        var activePost = document.querySelector('.post.active');
+        var nextPostToActive = activePost.nextElementSibling();
+
+        activePost.classList.remove('active');
+        nextPostToActive.classList.add('active');
+      },
+
+
+
+    };
 
 App.init();
